2eba16c86a4f17bd296864e0847bd2f153b2d138
Ralf Beckers <ralf.beckers@elektrobit.com>
2019-12-19 18:57:04 +0100

    3D

--- Rail/ViewModel/MainViewModel3D.cs
+++ Rail/ViewModel/MainViewModel3D.cs
@@ -18,62 +18,8 @@ namespace Rail.ViewModel
 
         public void Update3D()
         {
-            RenderPlate();
 
-
-            //TriangleNet.Geometry.Polygon polygon = new TriangleNet.Geometry.Polygon();
-            //polygon.Points.AddRange(this.RailPlan.PlatePoints.Select(p => new TriangleNet.Geometry.Vertex(p.X, p.Y)));
-            //TriangleNet.Mesh mesh = (TriangleNet.Mesh)(new TriangleNet.Meshing.GenericMesher()).Triangulate(polygon, null, null);
-
-            //List<TriangleNet.Topology.Triangle> triangles = mesh.Triangles.ToList();
-
-            //double centerX = this.RailPlan.PlatePoints.Max(p => p.X) / 2.0;
-            //double centerY = this.RailPlan.PlatePoints.Max(p => p.Y) / 2.0;
-
-            MeshGeometry3D plateGeometry3D = new MeshGeometry3D();
-            
-            plateGeometry3D.Positions.Add(new Point3D(-this.railPlan.Width / 2, -this.railPlan.Height / 2, 0));
-            plateGeometry3D.Positions.Add(new Point3D(+this.railPlan.Width / 2, -this.railPlan.Height / 2, 0));
-            plateGeometry3D.Positions.Add(new Point3D(-this.railPlan.Width / 2, +this.railPlan.Height / 2, 0));
-            plateGeometry3D.Positions.Add(new Point3D(+this.railPlan.Width / 2, +this.railPlan.Height / 2, 0));
-
-            plateGeometry3D.Normals.Add(new Vector3D(0, 0, 1));
-            plateGeometry3D.Normals.Add(new Vector3D(0, 0, 1));
-            plateGeometry3D.Normals.Add(new Vector3D(0, 0, 1));
-            plateGeometry3D.Normals.Add(new Vector3D(0, 0, 1));
-
-            plateGeometry3D.TextureCoordinates.Add(new Point(0, 1));
-            plateGeometry3D.TextureCoordinates.Add(new Point(1, 1));
-            plateGeometry3D.TextureCoordinates.Add(new Point(0, 0));
-            plateGeometry3D.TextureCoordinates.Add(new Point(1, 0));
-
-            plateGeometry3D.TriangleIndices.Add(0);
-            plateGeometry3D.TriangleIndices.Add(1);
-            plateGeometry3D.TriangleIndices.Add(2);
-
-            plateGeometry3D.TriangleIndices.Add(1);
-            plateGeometry3D.TriangleIndices.Add(3);
-            plateGeometry3D.TriangleIndices.Add(2);
-
-            this.PlateGeometry3D = plateGeometry3D;
-        }
-
-        private MeshGeometry3D plateGeometry3D;
-        public MeshGeometry3D PlateGeometry3D
-        {
-            get
-            {
-                return this.plateGeometry3D;
-            }
-            set
-            {
-                this.plateGeometry3D = value;
-                NotifyPropertyChanged("PlateGeometry3D");
-            }
-        }
-
-        public void RenderPlate()
-        {
+            // render plate image
             DrawingVisual drawingVisual = new DrawingVisual();
             using (DrawingContext drawingContext = drawingVisual.RenderOpen())
             {
@@ -86,16 +32,37 @@ namespace Rail.ViewModel
                 }));
 
                 this.RailPlan.Rails.ForEach(r => r.DrawRailItem(drawingContext, RailViewMode.Rail));
-                
+
             }
             RenderTargetBitmap bitmap = new RenderTargetBitmap(this.railPlan.Width, this.railPlan.Height, 96, 96, PixelFormats.Default);
             bitmap.Render(drawingVisual);
-
             this.PlateImage = bitmap;
+
+            // create 3D plate
+            Point3DCollection point3DCollection = new Point3DCollection()
+            {
+                new Point3D(-this.railPlan.Width / 2, -this.railPlan.Height / 2, 0),
+                new Point3D(+this.railPlan.Width / 2, -this.railPlan.Height / 2, 0),
+                new Point3D(-this.railPlan.Width / 2, +this.railPlan.Height / 2, 0),
+                new Point3D(+this.railPlan.Width / 2, +this.railPlan.Height / 2, 0)
+            };
+            this.PlatePoint3DCollection = point3DCollection;
         }
 
+        private Point3DCollection platePoint3DCollection;
+        public Point3DCollection PlatePoint3DCollection
+        {
+            get
+            {
+                return this.platePoint3DCollection;
+            }
+            set
+            {
+                this.platePoint3DCollection = value;
+                NotifyPropertyChanged(nameof(PlatePoint3DCollection));
+            }
+        }
         
-
         private ImageSource plateImage;
         public ImageSource PlateImage
         {
@@ -110,26 +77,32 @@ namespace Rail.ViewModel
             }
         }
 
-        //public static BitmapSource CreateBitmap(int width, int height, double dpi, Action<DrawingContext> render)
-        //{
-        //    DrawingVisual drawingVisual = new DrawingVisual();
-        //    using (DrawingContext drawingContext = drawingVisual.RenderOpen())
-        //    {
-        //        render(drawingContext);
-        //    }
-        //    RenderTargetBitmap bitmap = new RenderTargetBitmap(width, height, dpi, dpi, PixelFormats.Default);
-        //    bitmap.Render(drawingVisual);
-        //    return bitmap;
-        //}
-        
-//BitmapSource image = ImageTools.CreateBitmap(
-//            320, 240, 96,
-//            drawingContext =>
-//            {
-//                drawingContext.DrawRectangle(
-//                    Brushes.Green, null, new Rect(50, 50, 200, 100));
-//                drawingContext.DrawLine(
-//                    new Pen(Brushes.White, 2), new Point(0, 0), new Point(320, 240));
-//            });
+        private double rotationAngle = 0;
+        public double RotationAngle
+        {
+            get
+            {
+                return this.rotationAngle;
+            }
+            set
+            {
+                this.rotationAngle = value;
+                NotifyPropertyChanged(nameof(RotationAngle));
+            }
+        }
+
+        private double tiltAngle = -45;
+        public double TiltAngle
+        {
+            get
+            {
+                return this.tiltAngle;
+            }
+            set
+            {
+                this.tiltAngle = value;
+                NotifyPropertyChanged(nameof(TiltAngle));
+            }
+        }
     }
 }
